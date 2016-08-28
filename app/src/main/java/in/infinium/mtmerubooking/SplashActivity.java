5b1fdb3cef8cebcf4b61642de87303773b87fc7c
SamKapoor <sam.kap.ildc@gmail.com>
2016-08-28 12:24:11 +0530

    hide actionbar from splash

--- app/src/main/java/in/infinium/mtmerubooking/SplashActivity.java
+++ app/src/main/java/in/infinium/mtmerubooking/SplashActivity.java
@@ -1,22 +1,20 @@
 package in.infinium.mtmerubooking;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.os.Handler;
 
 import in.infinium.mtmerubooking.activity.LoginActivity;
 import in.infinium.mtmerubooking.utils.Common;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         executeSplash();
     }
